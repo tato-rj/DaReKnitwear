c81182c7ac4274685a392a88f8acbbb852b4c298
Arthur Villar <=>
2019-02-24 17:54:55 -0500

    Fix seeder

--- database/seeds/SweatersTableSeeder.php
+++ database/seeds/SweatersTableSeeder.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Database\Seeder;
 use App\{Sweater, Inventory, Image};
-
+use Faker\Factory as Faker;
 
 class SweatersTableSeeder extends Seeder
 {
+    protected $faker;
+
+    public function __construct()
+    {
+        $this->faker = Faker::create();
+    }
+
     public function run()
     {
         $this->newSweater(
@@ -144,7 +151,14 @@ class SweatersTableSeeder extends Seeder
                 $count = mt_rand(1,6);
 
                 for ($i=0; $i<$count; $i++) {
-                    $sweater->reviews()->create(factory('App\Review')->make());
+                    $sweater->reviews()->create([
+                        'name' => $this->faker->firstName . ' ' . $this->faker->lastName,
+                        'email' => $this->faker->freeEmail,
+                        'title' => $this->faker->sentence,
+                        'comment' => $this->faker->paragraph,
+                        'rating' => $this->faker->numberBetween(1,5),
+                        'created_at' => $this->faker->dateTimeBetween('-2 years' , 'now')
+                    ]);
                 }
             }
         }
