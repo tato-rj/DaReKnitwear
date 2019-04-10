68e85e3f770be4ac0dc34952c3534e697a3ce4f9
Arthur Villar <=>
2019-04-10 10:42:56 -0400

    Improve navbar changes on scroll

--- resources/js/components/scroll.js
+++ resources/js/components/scroll.js
@@ -12,8 +12,7 @@ $(window).scroll(function() {
     let scrollTop = $(this).scrollTop();
 
     if (scrollTop > limit) {
-        $header.removeClass('absolute-top').addClass('fixed-top shadow-sm');
-        $navbar.addClass('bg-white');
+        $header.removeClass('absolute-top').addClass('fixed-top shadow-sm bg-white');
         // $navItems.removeClass('p-3').addClass('p-2');
         // $logo.css('width', '120px');
         $logoText.css('font-size', '2.12em');
@@ -23,8 +22,7 @@ $(window).scroll(function() {
             $cookieAlert.show();
 
     } else {
-        $header.removeClass('fixed-top shadow-sm').addClass('absolute-top');
-        $navbar.removeClass('bg-white');
+        $header.removeClass('fixed-top shadow-sm bg-white').addClass('absolute-top');
         // $navItems.addClass('p-3').removeClass('p-2');
         // $logo.css('width', logoWidth);
         $logoText.css('font-size', '2.8em');
